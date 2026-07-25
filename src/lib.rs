316475bfbc4792e12b290b57b4f79b50e287dc6c
Alex Fedin <alex_fedin@hotmail.com>
2026-07-25 14:53:38 -0700

    Publish fft safe-uplift Rust mirror @ clang2rust 0.10.0-3299-g9fb6b28f

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,11 @@ mod fft;
 mod fft_h;
 
 extern "C" {
-    fn __transpiler_isa(child: i32, ancestor: i32)
-    -> bool;
-    fn __builtin_clzl(_: u64)
-    -> i32;
-    fn cos(_: f64)
-    -> f64;
-    fn sin(_: f64)
-    -> f64;
-    fn __builtin_clz(_: u32)
-    -> i32;
-    fn __builtin_clzll(_: u64)
-    -> i32;
-    fn __builtin_expect(_: i64, _: i64)
-    -> i64;
+    fn __transpiler_isa(child: i32, ancestor: i32) -> bool;
+    fn __builtin_clzl(_: u64) -> i32;
+    fn cos(_: f64) -> f64;
+    fn sin(_: f64) -> f64;
+    fn __builtin_clz(_: u32) -> i32;
+    fn __builtin_clzll(_: u64) -> i32;
+    fn __builtin_expect(_: i64, _: i64) -> i64;
 }
